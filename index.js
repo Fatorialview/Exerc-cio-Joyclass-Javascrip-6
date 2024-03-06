874c9a0edcf1ce21f38abfb5dfa79d129de2a2fd
Flavio <flaviosgp26@yahoo.com.br>
2024-03-06 11:31:31 -0300

    Exercício JavaScript curso Proz

---
+++ index.js
@@ -0,0 +1,14 @@
+let numA = 4 
+let numB = 5 
+
+let soma = `Soma entre ${numA} e ${numB} é ${numA + numB}`
+console.log(soma);
+
+let subtração = `Subtração entre ${numA} e ${numB} é ${numA - numB}`
+console.log(subtração);
+
+let multiplicação = `Multiplicação entre ${numA} e ${numB} é ${numA * numB}`
+console.log(multiplicação);
+
+let divisão = `Divisão entre ${numA} e ${numB} é ${numA / numB}`
+console.log(divisão);
